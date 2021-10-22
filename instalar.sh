aabef154c2e9cabf6c6adf774b991d801514c3b2
fredericovn <92985350+fredericovn@users.noreply.github.com>
2021-10-22 09:00:12 -0300

    Update instalar.sh

--- instalar.sh
+++ instalar.sh
@@ -1,14 +1,10 @@
 #! /bin/sh
 cd /home/pi
-sudo rm -rf LCD-show
-sudo rm -rf raspi
+rm -rf LCD-show
+rm -rf raspi
 git clone https://github.com/Lcdwiki/LCD-show
-git clone https://github.com/fredericovn/raspi
 chmod -R 755 LCD-show
-chmod -R 755 raspi
-cd raspi
 cp kiosk.service /lib/systemd/system/
 systemctl enable kiosk.service
-cd ..
 cd LCD-show
 ./MHS35-show
