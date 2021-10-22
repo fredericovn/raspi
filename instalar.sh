be11fefe54176d16e1d88ec658239f617e9f95a1
fredericovn <92985350+fredericovn@users.noreply.github.com>
2021-10-22 10:03:16 -0300

    Add files via upload

--- instalar.sh
+++ instalar.sh
@@ -1,13 +1,9 @@
 #! /bin/sh
-cd /home/pi
 rm -rf LCD-show
 git clone https://github.com/Lcdwiki/LCD-show
 chmod -R 755 LCD-show
-chmod -R 755 raspi
-cd raspi
-sudo cp kiosk.sh home.pi
+sudo cp kiosk.sh /home/pi
 sudo cp kiosk.service /lib/systemd/system/
 sudo systemctl enable kiosk.service
-cd..
 cd LCD-show
 ./MHS35-show
